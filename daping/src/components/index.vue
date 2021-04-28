31728fc3b68c536ccf1eddf17cda094111ba1094
周弋杰 <zhou18625526707@163.com>
2021-04-28 19:51:04 +0800

    完成底部两个图表

---
+++ daping/src/components/index.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="wrap">
+        <header class="e-header"></header>
+        <echarts-map></echarts-map>
+        <div class="line-bar clear">
+            <chargdataanalysis></chargdataanalysis>
+            <chargtransactionstatistics></chargtransactionstatistics>
+        </div>
+    </div>
+</template>
+<script>
+import EchartsMap from './EchartsMap.vue';
+import Chargdataanalysis from './Chargdataanalysis.vue';
+import Chargtransactionstatistics from './Chargtransactionstatistics.vue';
+
+export default {
+    name:'Cgdataanalysis',
+    data(){
+        return{
+            myChart:'',
+            xData:'',
+        }
+    },
+    components:{
+        EchartsMap,
+        Chargtransactionstatistics,
+        Chargdataanalysis,
+    },
+    mounted(){
+        
+    },
+    methods:{
+        
+
+    }
+}
+</script>
+<style scoped>
+.wrap{
+    background:url(../assets/bg-01.jpg) center no-repeat;
+    background-size: 100% 100%;
+}
+.clear{
+    overflow: hidden;
+}
+.app-vue{
+    width: 300px;
+    height: 200px;
+    background-color: gold;
+}
+.e-header{
+    width: 100%;
+    height: 85px;
+}
+.line-bar{
+    padding: 0 40px 36px;
+}
+</style>
--- daping/src/main.js
+++ daping/src/main.js
@@ -3,9 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import * as echarts from 'echarts';
+import './assets/china'
 Vue.config.productionTip = false
-
+Vue.prototype.$echarts = echarts
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
